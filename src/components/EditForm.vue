28e17cf3751a18de2e0eda87c25dd7d5999647fd
yichengjie <626659321@qq.com>
2016-12-14 17:52:25 +0800

    路由测试

---
+++ src/components/EditForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <form>
+        <div>
+            <input type="text" v-model ="name">
+        </div>
+    </form>
+</template>
+<script>
+    export default{
+      data () {
+        return {
+          name: ''
+        }
+      }
+    }
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>User {{ $route.params.id }}</div>
+</template>
+<script>
+    export default {
+      watch: {
+        '$route' (to, from) {
+          // 对路由变化作出响应...
+          console.info('to : ', to)
+          console.info('from : ', from)
+        }
+      }
+    }
+</script>
