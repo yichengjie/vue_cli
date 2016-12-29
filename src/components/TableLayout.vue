dca0a1941b56b5cff93c46168248d44cd86e1e47
yichengjie <626659321@qq.com>
2016-12-29 15:25:42 +0800

    备份

---
+++ src/components/TableLayout.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  props:{
+    titleObj:Object,
+    list:Array
+  },
+  render(h) {
+    let thead = h('thead', [h('tr',getThsByTr(this.titleObj,h))]) ;
+    let tbody = h('tbody',getTrsByList(this.titleObj,this.list,h)) ;
+    let table = h('table',{
+      attrs:{
+        class:'table table-bordered'
+      }
+    },[thead,tbody]) ;
+    return table ;
+  }
+} ;
+//遍历表头中的每一列
+function getThsByTr (titleObj,h) {
+    let keys = Object.keys(titleObj) ;
+    return keys.map(key =>{
+      return h('th',titleObj[key]) ;
+    }) ;
+}
+//遍历tr中的每一列
+function getTdsByTr (keys,item,h){
+  return keys.map(key => {
+    return h('td',item[key]) ;
+  }) ;
+}
+//遍历表格中的每一行
+function getTrsByList (titleObj,list,h) {
+  let keys = Object.keys(titleObj) ;
+  return list.map(item => {
+      return h('tr',getTdsByTr(keys,item,h)) ;
+  }) ;
+}
+</script>
--- src/views/query/QueryList.vue
+++ src/views/query/QueryList.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="query-list" v-if="list.length>0">
-      <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <QueryListItem v-for ="item in list" :item="item"/>
-          </tbody>
-    </table>
+      <TableLayout :titleObj="titleObj" :list="list" />
+
   </div>
 </template>
 <script>
+  import TableLayout from 'components/TableLayout.vue' ;
   import QueryListItem from './QueryListItem.vue' ;
   export default {
     props:{
       list:Array
     },
     components:{
-      QueryListItem
+      QueryListItem,
+      TableLayout
+    },
+    data () {
+      return {
+        titleObj:{
+          id:'#',
+          name:'姓名',
+          classes:'班级',
+          dept:'系列'
+        }
+      } ;
     }
   } ;
 </script>
