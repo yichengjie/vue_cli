c79c0113800b9cd25642dc711828bad9351a10e8
yichengjie <626659321@qq.com>
2016-12-30 16:03:44 +0800

    备份

---
+++ src/views/edit/EditForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <OCForm>
+     <OCFormItem label="姓名">
+        <OCInput v-model ="formData.name" />
+     </OCFormItem>
+     <OCFormItem label="地址">
+        <OCInput v-model ="formData.addr" />
+     </OCFormItem>
+  </OCForm>
+</template>
+<script>
+  import OCForm from 'components/edit/form.vue' ;
+  import OCFormItem from 'components/edit/form-item.vue' ;
+  import OCInput from 'components/edit/input.vue' ;
+  export default {
+    props:{
+      formData:Object
+    },
+    components:{
+      OCForm,
+      OCFormItem,
+      OCInput
+    }
+  } ;
+</script>
+<style>
+  .edit-form {
+    margin-top: 20px;
+  }
+</style>
---
+++ src/views/edit/MainContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-content">
+    <QueryCondation :formData="formData"/>
+    <EditForm :formData="formData"/>
+  </div>
+</template>
+<script>
+  import EditForm from './EditForm.vue' ;
+  import QueryCondation from './QueryCondation.vue' ;
+  export default {
+    components:{
+      EditForm,
+      QueryCondation
+    },
+    data() {
+      return {
+        formData:{
+          name:'',
+          addr:''
+        }
+      } ;
+    }
+  } ;
+</script>
+<style>
+  .main-content{
+    margin: 0px;
+  }
+</style>
