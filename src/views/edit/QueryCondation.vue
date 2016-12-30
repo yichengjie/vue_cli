0ac60f73554ee6c7b5745fb2f276992ad2ef1803
yichengjie <626659321@qq.com>
2016-12-30 16:07:06 +0800

    备份

--- src/views/edit/QueryCondation.vue
+++ src/views/edit/QueryCondation.vue
@@ -2,6 +2,7 @@
   <div class ="query_section">
     <QueryRowLayout>
         <span class="pull-right marginR15">
+            <button type="button" class="btn btn-sm" @click ="handleBackOper">返回</button>
             <button type="button" class="btn btn-sm btn-primary" @click ="handleQueryOper">保存</button>
         </span>
     </QueryRowLayout>
@@ -17,6 +18,9 @@
       QueryRowLayout
     },
     methods:{
+      handleBackOper(){
+        console.info('返回上一个页面...') ;
+      },
       handleQueryOper(){
         console.info('你点击了保存按钮,表单数据为 : ' + JSON.stringify(this.formData)) ;
         console.info('第一步 : 数据合法性校验') ;
