a231e785bce24b86db62e978b8421b0632b251a9
yichengjie <626659321@qq.com>
2017-01-04 10:33:32 +0800

    增加todos demo

---
+++ src/views/todos/todo-eidt.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="todo-edit">
+      <div class="row">
+        <div class="col-sm-offset-4 col-sm-8">
+           <h2>{{pageTitle}}</h2>
+        </div>
+      </div>
+      <form class="form-horizontal" role="form">
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-4 control-label">First Name</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" v-model="formData.firstName" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="inputPassword3" class="col-sm-4 control-label">Last Name</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" v-model="formData.lastName" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="inputPassword3" class="col-sm-4 control-label">Username</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" v-model ="formData.userName" />
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-4 col-sm-8">
+            <button type="button" class="btn btn-default" @click ="handleSaveOper">保存</button>
+          </div>
+        </div>
+      </form>
+  </div>
+</template>
+<script>
+  export default {
+    props:{
+      formData:Object,
+      newAddUser:Function,
+      updateUser:Function
+    },
+    computed:{
+      pageTitle(){
+        if(this.formData.id&&this.formData.id.length>0){
+          return "修改用户信息" ;
+        }else{
+          return "新增用户信息" ;
+        }
+      }
+    },
+    methods:{
+      handleSaveOper(event){
+        console.info('formData : ' ,JSON.stringify(this.formData)) ;
+        let id = this.formData.id ;
+        if(id&&id.length>0){
+          this.updateUser(this.formData) ;
+        }else{
+           this.newAddUser(this.formData) ;
+        }
+        //重置现有的表单填写数据
+        this.resetForm() ;
+      },
+      resetForm(){/*重置表单*/
+        var emptyFormData = { id:'',firstName:'',lastName:'',userName:''} ;
+        Object.assign(this.formData,emptyFormData) ;
+      }
+    }
+  } ;
+</script>
